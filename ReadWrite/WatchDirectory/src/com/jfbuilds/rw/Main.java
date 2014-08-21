74fc6bd575730653a8fe0be328fce923e85a260c
Jean-francois Nepton <jeanmfrancois@gmail.com>
2014-08-21 02:46:53 -0400

    Complete Lesson on Read/ Write

--- ReadWrite/WatchDirectory/src/com/jfbuilds/rw/Main.java
+++ ReadWrite/WatchDirectory/src/com/jfbuilds/rw/Main.java
@@ -1,8 +1,39 @@
 package com.jfbuilds.rw;
 
+import java.nio.file.*;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
 	public static void main(String[] args) {
+        try (WatchService service = FileSystems.getDefault().newWatchService()) {
 
+            Map<WatchKey,Path> keyMap = new HashMap<>();
+
+            Path path = Paths.get("WatchDirectory/files");
+
+            keyMap.put(path.register(service,
+                    StandardWatchEventKinds.ENTRY_CREATE,
+                    StandardWatchEventKinds.ENTRY_DELETE,
+                    StandardWatchEventKinds.ENTRY_MODIFY),
+                    path);
+
+            WatchKey watchKey;
+
+            do {
+                watchKey = service.take();
+                Path eventDir = keyMap.get(watchKey);
+
+                for (WatchEvent<?> event : watchKey.pollEvents()) {
+                    WatchEvent.Kind<?> kind = event.kind();
+                    Path eventPath = (Path) event.context();
+                    System.out.println(eventDir + ": " + kind + ": " + eventPath);
+                }
+            } while (watchKey.reset());
+
+        } catch (Exception e) {
+            e.getMessage();
+        }
 	}
 }
