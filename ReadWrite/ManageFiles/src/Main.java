859ca80d6f8e05c8b8463d6667d0064ad5621271
Jean-francois Nepton <jeanmfrancois@gmail.com>
2014-08-20 22:25:09 -0400

    Complete File Management Topic

--- ReadWrite/ManageFiles/src/Main.java
+++ ReadWrite/ManageFiles/src/Main.java
@@ -1,13 +1,27 @@
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		
-		Path source = Paths.get("files/loremipsum.txt");
+		Path source = Paths.get("ManageFiles/files/loremipsum.txt");
 		System.out.println(source.getFileName());
-	}
+
+        Path target = Paths.get("ManageFiles/files/newfile.txt");
+        Files.copy(source,target, StandardCopyOption.REPLACE_EXISTING);
+
+//        Path toDelete = Paths.get("ManageFiles/files/todelete.txt");
+//        Files.delete(toDelete);
+
+        Path newdir = Paths.get("ManageFiles/files/newdir");
+        Files.createDirectory(newdir);
+
+        Files.move(source, newdir.resolve(source.getFileName()),
+                StandardCopyOption.REPLACE_EXISTING);
+    }
 
 }
