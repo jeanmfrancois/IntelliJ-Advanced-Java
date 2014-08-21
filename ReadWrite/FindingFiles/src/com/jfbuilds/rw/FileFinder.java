2c8fa3c365a3db69f88945db9b2cd5b029f0f14f
Jean-francois Nepton <jeanmfrancois@gmail.com>
2014-08-21 02:11:43 -0400

    Complete Find Files Topic

--- ReadWrite/FindingFiles/src/com/jfbuilds/rw/FileFinder.java
+++ ReadWrite/FindingFiles/src/com/jfbuilds/rw/FileFinder.java
@@ -1,19 +1,28 @@
 package com.jfbuilds.rw;
 
 import java.io.IOException;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Path;
-import java.nio.file.SimpleFileVisitor;
+import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 
 public  class FileFinder extends SimpleFileVisitor<Path> {
 
-	public FileFinder() {
+    private PathMatcher matcher;
+    public ArrayList<Path> foundPaths = new ArrayList<>();
+
+	public FileFinder(String pattern) {
+        matcher = FileSystems.getDefault().getPathMatcher("glob:" + pattern); //("regex:" + pattern) if looking for regular expression pattern
 	}
 	
 	@Override
 	public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
 			throws IOException {
-		return FileVisitResult.CONTINUE;
+
+        Path name = file.getFileName();
+        System.out.println("Examining " + name);
+        if (matcher.matches(name)) {
+            foundPaths.add(file);
+        }
+        return FileVisitResult.CONTINUE;
 	}
 }
--- ReadWrite/FindingFiles/src/com/jfbuilds/rw/Main.java
+++ ReadWrite/FindingFiles/src/com/jfbuilds/rw/Main.java
@@ -1,15 +1,34 @@
 package com.jfbuilds.rw;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 
-		Path fileDir = Paths.get("files");
+		Path fileDir = Paths.get("FindingFiles/files");
 
+        FileFinder finder = new FileFinder("file3.txt");
+        Files.walkFileTree(fileDir, finder);
+
+        ArrayList<Path> foundFiles = finder.foundPaths;
+
+        if (foundFiles.size() > 0) {
+            foundFiles.forEach( p -> {
+                try {
+                    System.out.println(p.toRealPath(LinkOption.NOFOLLOW_LINKS));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        } else {
+            System.out.println("No files were found!");
+        }
 	}
 	
 }
